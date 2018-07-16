9f802b274da64d559eeb5e6b369f33a8833d2565
oishidass <oishidass1234@gmail.com>
2018-07-16 14:38:06 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -9,11 +9,12 @@ item = gets.chomp
 cost = gets.chomp
   items << item
   price << cost
+end 
 
 puts "Are you an Amazon employee?"
   if answer = yes
     price.each do |discount|
-      price - (price*20) = total
+      price - (price*.20) = total
   else
     puts total
   end
