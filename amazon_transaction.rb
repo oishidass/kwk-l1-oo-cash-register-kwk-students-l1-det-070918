01b130c93bb51b5349b3b90515406743790eb86c
oishidass <oishidass1234@gmail.com>
2018-07-16 14:11:59 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,13 +1,15 @@
 class AmazonTransaction
- end  
-amazon_transaction  AmazonTransaction.new 
+  amazon_transaction = AmazonTransaction.new 
+  
 
 
 amazon_transaction_with_discount
  AmazonTransaction.new
  
  def initialize 
-   @ total = 0 
+   @total = 0 
+   amazon_transaction.new (amazon_transaction)
+   
    
    
  
