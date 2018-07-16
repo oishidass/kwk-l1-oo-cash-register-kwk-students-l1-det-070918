9951e5de8da075ad911a2e7c0dc66f50569f5408
oishidass <oishidass1234@gmail.com>
2018-07-16 14:29:15 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -5,7 +5,8 @@ puts "hey welcome to amazon! feel free to buy whatever you like"
 sleep (1)
 
 puts "so what would you like to buy and how much does it cost?"
-item, price = gets.chomp 
+item = gets.chomp 
+price = gets.chomp
   items << {#item}
   price << {#price}
   puts items
